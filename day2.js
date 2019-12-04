e9c6978cc817c7b58c8389610327f05822cad6bf
Sam Parkinson <parkinsonsp42@gmail.com>
2019-12-04 14:09:25 -0500

    Solved day 2 problem 1

--- day2.js
+++ day2.js
@@ -7,3 +7,31 @@
   Other than 1, 2, 99: something went wrong
   Before performing any operations, replace [1] with '12' and [2] with '2'
 */
+
+function intcodeComp(opArr) {
+  // set to initial state
+  opArr[1] = 12;
+  opArr[2] = 2;
+  // track position
+  // for loop here represents the intcode computer, increment in steps of 4
+  for (let i = 0; i < (opArr.length - 4); i += 4) {
+    if (opArr[i] === 99) {
+      break
+    } else {
+      const j = opArr[i + 1];
+      const k = opArr[i + 2];
+      const l = opArr[i + 3];
+      if (opArr[i] === 1) {
+        opArr[l] = opArr[j] + opArr[k];
+      } else if (opArr[i] === 2) {
+        opArr[l] = opArr[j] * opArr[k];
+      }
+    } 
+  }
+
+  return opArr[0];
+}
+
+module.exports = {
+  intcodeComp,
+}
